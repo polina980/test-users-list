c41a5b80c7a6b8415e540049f30d6afc8a482614
PolinaShchepochkina <polina007@inbox.ru>
2023-06-12 12:53:23 +0300

    add protectedroute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from '../../pages/LoginPage';
-import MainPage from '../../pages/MainPage';
-import UserPage from '../../pages/UserPage';
+import LoginPage from '../../pages/LoginPage.jsx';
+import MainPage from '../../pages/MainPage.jsx';
+import UserPage from '../../pages/UserPage.jsx';
+import ProtectedRoute from '../../utils/router/ProtectedRoute.js';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/users" element={<MainPage />} />
-      <Route path="/about" element={<UserPage />} />
+      <Route path='/' element={<LoginPage />} />
+      <Route
+        path='/users'
+        element={
+          <ProtectedRoute >
+            <MainPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path='/about'
+        element={
+          <ProtectedRoute >
+            <UserPage />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
-  )
+  );
 }
 
 export default App;
---
+++ src/utils/router/ProtectedRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children, fallbackPath = '/' }) => {
+  const isLoggedIn = localStorage.getItem('email') && localStorage.getItem('password');
+
+  if (!isLoggedIn) {
+    return <Navigate to={fallbackPath} replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
