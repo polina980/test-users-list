c0612e22b19667e2dc9eccfd12aeda7d91ee385f
PolinaShchepochkina <polina007@inbox.ru>
2023-07-14 13:47:20 +0300

    change router for deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
